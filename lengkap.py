7296a7b77eb61bd0ea6f307d2341a4edee922383
dionne22 <dionne.aurelia@gmail.com>
2022-02-17 01:44:55 +0700

    Update lengkap.py

--- lengkap.py
+++ lengkap.py
@@ -1,76 +1,66 @@
-import sys
-import random
+
 import codecs
+
+
+def shortenKey(key): #memendekkan panjang key menjadi 256 bit
+    temp_key = list(key)
+    new_key = []
+    for i in range(32):
+        new_key.append(temp_key[i])
+    return new_key
 
 def ksa_lsfr(key):
     key_length = len(key)
-    key = list(key)
-    for i in range(len(key)):
-        key[i] = ord(key[i])
+
+    if(len(key)<=32):  #mengulang key jika panjang key kurang dari 256 bit
+        key = list(key)
+        for i in range(len(key)):
+            key[i] = ord(key[i])
+        for i in range(32 - len(key)): 
+            key.append(key[i % len(key)])
+    else: #memendekkan panjang key menjadi 256 bit
+        shortenKey(key)
+
+    #LSFR    
     lsfr = []
     biner = ''.join(format(i, '08b') for i in key)
     l = len(biner)
     for i in range(l):
         lsfr.append(biner[l-1])
-        biner = str(int(biner[0]) ^ int(biner[l-1])) + biner[0:l-1]
+        biner = str( int(biner[l-1]) ^ int(biner[0])) + biner[0:l-1]
 
-    # ksa = list()
-    # for c in key :
-    #     ksa.append(ord(c))
-    # panjang_key = len(ksa)
-
-    # S = list(range(256))
-    # j = 0
-    # for i in range(256):
-    #     j = (j + S[i] + ksa[i % panjang_key]) % 256
-    #     S[i], S[j] = S[j], S[i]  # swap
-
-
-    # panjang_ksa = len(key)
-
+    #KSA
     S = list(range(256))
     j = 0
     for i in range(256):
-        j = (j + S[i] + key[i % key_length] + int(ord(key[i % key_length])) + int(lsfr[i])) % 256
-        S[i], S[j] = S[j], S[i]
+        j = (j + S[i] + int(lsfr[i]) + int((key[i % key_length]))) % 256
+        S[i], S[j] = S[j], S[i] 
 
     return S
 
-def PRGA(plainteks,S):
+def PRGA(S):
     i = 0
     j = 0
-    for i in range (len(plainteks)-1):
+    while True:
         i = (i+1)%256
         j = (j+S[i])%256
         S[i], S[j] = S[j], S[i]
         t = (S[i]+S[j])%256
         u = S[t] #keystream
-    return u
+        yield u
         # c = u^P[idx]
 
-def encrypt(plainteks, key):
-    # key = [ord(c) for c in key]
-    S = ksa_lsfr(key)
-    kstream = PRGA(S)
-    output = []
-    for char in plainteks:
-        output.append("%02X" % (ord(char)^next(kstream)))
-    hexd = ''.join(output)
-    hexdata = hexd.encode('ascii').strip()
-    return hexdata
+def convert(key, teks): # Encrypt & decrypt
+    input = list()
+    for c in teks :
+        input.append(ord(c))
+    keystream = PRGA(ksa_lsfr(key))
 
-def dec(cipherteks,key):
-    key = [ord(c) for c in key]
-    S = ksa_lsfr(key)
-    kstream = PRGA(S)
-    output = []
-    for char in cipherteks:
-        output.append("%02X" % (ord(char)^next(kstream)))
-    hexd = ''.join(output)
-    hexd = hexd.encode('ascii')
-    decode_hex = codecs.getdecoder('ascii')
-    hexd = decode_hex(hexd)[0]
-    hexdata = hexd.strip()
-    return hexdata
+    result = []
+    for i in input:
+        r = ("%02X" % (i ^ next(keystream)))    
+        result.append(r)
+    output = ''.join(result)
+    return (codecs.decode(output, 'hex_codec').decode('latin-1'))
 
-# print(encrypt('senin','selasa'))
+# print(convert('senin','uæá1Þ¶bû3ï"'))
