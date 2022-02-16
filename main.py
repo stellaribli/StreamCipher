3709c661884a919f0366ac5b33299b49c366c10c
Stella Ribli <18219027@std.stei.itb.ac.id>
2022-02-17 00:02:41 +0700

    ui change

--- main.py
+++ main.py
@@ -23,11 +23,54 @@ class Landing(QDialog):
     def __init__(self):
         super(Landing, self).__init__()
         loadUi('landingpage.ui', self)
-        # self.vigButton.clicked.connect(self.gotoVig)
-
+        self.teksButton.clicked.connect(self.gotoText)
+        self.uploadButton.clicked.connect(self.gotoUpload)
+    def gotoText(self):
+        widget.setCurrentIndex(2)
+    def gotoUpload(self):
+        widget.setCurrentIndex(1)
+class Text(QDialog):
+    def __init__(self):
+        super(Text, self).__init__()
+        loadUi('text.ui', self)
+        self.encryptB.clicked.connect(self.encrypt)
+        self.decryptB.clicked.connect(self.decrypt)
+        self.uploadedFile = None 
+    def encrypt(self): 
+        kunci = self.kunci.text()
+        teks = self.teks.text() 
+        self.hasil.setText('keluaran')   
+    def decrypt(self):    
+        kunci = self.kunci.text()
+        teks = self.teks.text() 
+        self.hasil.setText('keluaranx')   
+class Upload(QDialog):
+    def __init__(self):
+        super(Upload, self).__init__()
+        loadUi('upload.ui', self)
+        self.uploadButton.clicked.connect(self.upload)
+        self.encryptB.clicked.connect(self.encrypt)
+        self.decryptB.clicked.connect(self.decrypt)
+        self.uploadedFile = None 
+    def encrypt(self): 
+        kunci = self.kunci.text()
+        self.hasil.setText('keluaran')   
+    def decrypt(self):    
+        kunci = self.kunci.text()
+        self.hasil.setText('keluaranx')   
+    def upload(self):
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "Upload CV File", "")
+        if fileName:
+            self.uploadedFile = fileName
+            self.fileName.setText(os.path.basename(fileName))
+        else:
+            print("No file selected")  
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(Landing())  # Index jadi 0   
+widget.addWidget(Landing())  # Index jadi 0  
+widget.addWidget(Upload())  # Index jadi 1 
+widget.addWidget(Text())  # Index jadi 2
 widget.setCurrentIndex(0)
 widget.setFixedWidth(780)
 widget.setFixedHeight(319)
